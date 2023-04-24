69e806ecc18f68dbbc8afc6927be97a2dd82d75f
TetieWasTaken <tjvssr@gmail.com>
2023-04-24 20:48:09 +0200

    feat: add color option

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,64 @@
 using namespace std;
 using namespace cv;
 
-char intensityToChar(int intensity)
+string toLower(string input)
+{
+    string output = "";
+    for (int i = 0; i < input.length(); i++)
+    {
+        output += tolower(input[i]);
+    }
+    return output;
+}
+
+bool validateInput(string input)
+{
+    if (input.empty() || toLower(input) == string("n") || toLower(input) == string("y"))
+    {
+        return true;
+    }
+    return false;
+}
+
+/* char intensityToChar(int intensity)
 {
     const string CHARSET = "@#MW%*oahkbdpwmZO0QYPXzvxnrt|(){}[]/?-+~^<>i!lI;:,\"^`'. ";
     int index = (int)(CHARSET.length() * (intensity) / 255);
     return CHARSET[index];
+} */
+
+string colorToChar(Vec3b color, bool withColor)
+{
+    const string CHARSET = "@#MW%*oahkbdpwmZO0QYPXzvxnrt|(){}[]/?-+~^<>i!lI;:,\"^`'. ";
+    int intensity = (color[0] + color[1] + color[2]) / 3;
+    int index = (int)(CHARSET.length() * (intensity) / 255);
+    if (withColor)
+    {
+        return "\033[38;2;" + to_string(color[2]) + ";" + to_string(color[1]) + ";" + to_string(color[0]) + "m" + CHARSET[index] + "\033[0m";
+    }
+
+    return string(1, CHARSET[index]);
 }
 
 int main(int argc, char **argv)
 {
+    bool withColor = false;
+    string userInput;
+
+    // Prompt the user for color or grayscale
+    cout << "Do you want color? (y/N): ";
+    getline(cin, userInput);
+
+    if (validateInput(userInput))
+    {
+        withColor = toLower(userInput) == string("y");
+    }
+    else
+    {
+        cout << "Input must be one of: y, n" << endl;
+        return -1;
+    }
+
     VideoCapture cap(0);
     if (!cap.isOpened())
     {
@@ -37,7 +86,7 @@ int main(int argc, char **argv)
         {
             for (int j = 0; j < frame.cols; j++)
             {
-                ascii += intensityToChar(frame.at<uchar>(i, j));
+                ascii += colorToChar(frame.at<Vec3b>(i, j), withColor);
             }
             ascii += "\n";
         }

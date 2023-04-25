0f38419f50249e406c8d48735ac8d1e0f3e35b0b
TetieWasTaken <tjvssr@gmail.com>
2023-04-25 12:07:14 +0200

    chore: rework validation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <mach/mach.h>
 #include <chrono>
+#include "errorcodes.h"
 
 using namespace std;
 using namespace cv;
@@ -10,6 +11,14 @@ using namespace cv;
 */
 
 #define clock chrono::steady_clock
+
+enum paramType
+{
+    INT,
+    STRING,
+    BOOL
+};
+
 const string CHARSET = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@";
 
 char intensityToChar(int intensity)
@@ -18,7 +27,33 @@ char intensityToChar(int intensity)
     return CHARSET[index];
 }
 
-int getParam(string message, int defaultValue)
+bool validateParam(string value, paramType type)
+{
+    if (type == INT)
+    {
+        for (int i = 0; i < value.length(); i++)
+        {
+            if (!isdigit(value[i]))
+            {
+                throw InvalidationError(InvalidArgument, "Invalid argument", value);
+            }
+        }
+        return true;
+    }
+    else if (type == STRING)
+    {
+        return true;
+    }
+    else if (type == BOOL)
+    {
+        if ((value != "y") && (value != "Y") && (value != "n") && (value != "N"))
+            throw InvalidationError(InvalidArgument, "Invalid argument", value);
+        return true;
+    }
+    throw InvalidationError(InvalidArgument, "Invalid argument", value);
+}
+
+int getIntParam(string message, int defaultValue)
 {
     string valueStr;
     cout << message << " (" << defaultValue << "): ";
@@ -27,9 +62,37 @@ int getParam(string message, int defaultValue)
     {
         return defaultValue;
     }
+    validateParam(valueStr, INT);
     return stoi(valueStr);
 }
 
+string getStringParam(string message, string defaultValue)
+{
+    string valueStr;
+    cout << message << " (" << defaultValue << "): ";
+    getline(cin, valueStr);
+    if (valueStr == "")
+    {
+        return defaultValue;
+    }
+    validateParam(valueStr, STRING);
+    return valueStr;
+}
+
+bool getBoolParam(string message, bool defaultValue)
+{
+    string valueStr;
+    cout << message << " (" << (defaultValue ? "Y/n" : "y/N") << "): ";
+    getline(cin, valueStr);
+    if (valueStr == "")
+    {
+        return defaultValue;
+    }
+    validateParam(valueStr, BOOL);
+    return valueStr == "y" || valueStr == "Y";
+}
+
+string logInfo(clock::time_point started_at)
 {
     mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
     vm_statistics_data_t vmstat;
@@ -66,11 +129,12 @@ int main(int argc, char **argv)
 {
     clock::time_point started_at = clock::now();
 
-    int timeBetweenFrames = getParam("Time between frames (ms)", 25);
-    int xSize = getParam("X size", 160);
-    int ySize = getParam("Y size", 120);
 
     while (1)
+    bool useWebcam = getBoolParam("Webcam", true);
+    int xSize = getIntParam("X size", 160);
+    int ySize = getIntParam("Y size", 120);
+    if (useWebcam)
     {
         Mat frame;
         cap >> frame;
@@ -83,6 +147,8 @@ int main(int argc, char **argv)
 
         string ascii = "";
         for (int i = 0; i < frame.rows; i++)
+    else
+    {
         {
             for (int j = 0; j < frame.cols; j++)
             {
